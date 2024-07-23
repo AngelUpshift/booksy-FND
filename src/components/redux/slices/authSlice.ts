463949314b92320ce4de175054641fdd55dbc6ba
Natasha Ilievska <natasha.ilievska@upshiftwork.com>
2024-07-23 08:57:47 +0200

    .

--- src/components/redux/slices/authSlice.ts
+++ src/components/redux/slices/authSlice.ts
@@ -12,3 +12,17 @@ const initialState: UserState = {
   isLoading: false,
   error: null,
 };
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    register: (state, action) => {},
+    login: (state, action) => {},
+    logout: (state, action) => {},
+    me: (state, action) => {},
+    changePassword: (state, action) => {},
+    forgotPassword: (state, action) => {},
+    resetPassword: (state, action) => {},
+  },
+});
